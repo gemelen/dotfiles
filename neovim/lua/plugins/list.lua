15e83260c078e1e8a8c70f346b1766d60a562d86
Denis Pyshev <git@gemelen.net>
2024-01-26 12:37:15 -0300

    Switch to the Neotree 3.x

--- neovim/lua/plugins/list.lua
+++ neovim/lua/plugins/list.lua
@@ -27,7 +27,7 @@ return require("packer").startup(function(use)
         -- file browser
         use({
           "nvim-neo-tree/neo-tree.nvim",
-            branch = "v2.x",
+            branch = "v3.x",
             requires = { 
               "nvim-lua/plenary.nvim",
               "kyazdani42/nvim-web-devicons",
