d2344cb8bf41a9f68b071e84ceb39d7ae2e3568d
Denis Pyshev <git@gemelen.net>
2024-02-26 11:49:46 -0300

    Move lockfile out of main config dir

--- neovim/lua/setup/lazy.lua
+++ neovim/lua/setup/lazy.lua
@@ -1,4 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazyopts = {
+  lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json"
+}
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -11,4 +14,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", lazyopts)
